133b06b820047a29fbc98ff4ffc93401449a3027
Rade <you@example.com>
2021-02-26 20:32:35 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+// SHOWING ELEMENTS
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+
+
+
+
+
+
+// ZGOLEMUVANE NA BROJ
 const broj = document.querySelectorAll('.broj')
 
 broj.forEach(broj => {
